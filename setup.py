af6527160d2c0c0f61d737f383e35fd767ce25be
Vincent Pelletier <plr.vincent@gmail.com>
2016-12-13 13:27:37 +0000

    Bump to 1.2 .
    Also, update classifiers (especially denoting Python 3 support).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='hidraw-pure',
     description='Pure-python linux hidraw bindings',
     keywords='linux human interface device HID hidraw',
-    version='1.1',
+    version='1.2',
     author='Vincent Pelletier',
     author_email='plr.vincent@gmail.com',
     url='http://github.com/vpelletier/python-hidraw',
@@ -12,8 +12,11 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU General Public License (GPLv2+)',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Hardware :: Hardware Drivers',
     ],
     install_requires=[
         'ioctl-opt',
